53e1c3cd31eb7d387edd9137aa9ea6699eb1f05d
David Estephanian <desteph@protonmail.com>
2022-02-22 15:33:22 -0800

    started implementing stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 
+// I: Object
+// O: String
+// C:
+// E:
+
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+  // Create a result string.
+  var stringifyResult = '';
+
+  console.log(var + 'dsfsdf' + var2);
+
+  // For static variables.
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    stringifyResult += obj;
+  }
+
+  // Danny's implementation of string (see if this works !)
+  if (typeof obj === 'string') {
+    var stringifyResult = obj.filter(x => x !== '"');
+  }
+
+  // Iterate through the object parameter.
+  for (var key in obj) {
+    // If the object value is a function or undefined, return '{}'.
+    if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+      return '{}';
+    }
+  }
+  // Return result string.
+  return stringifyResult;
 };
